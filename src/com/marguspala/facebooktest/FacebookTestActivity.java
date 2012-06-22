e6383851b139758ed005f255295c544c049d6f38
jaspermogg <saganfan162@gmail.com>
2012-06-22 18:18:13 +0100

    Deletes app data on install.

--- src/com/marguspala/facebooktest/FacebookTestActivity.java
+++ src/com/marguspala/facebooktest/FacebookTestActivity.java
@@ -2,12 +2,63 @@ package com.marguspala.facebooktest;
 
 import org.apache.cordova.DroidGap;
 import android.os.Bundle;
+//required for cleaning out appData each time app starts.
+import java.io.File;
+import android.util.Log;
 
 public class FacebookTestActivity extends DroidGap {
-    /** Called when the activity is first created. */
+    /** Called when the activity is first created. */	
+	
+	
+    //remove this line in production
+	private static FacebookTestActivity instance;
+	
+	//keep
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	 
+         //remove these lines in production
+		instance = this;
+        FacebookTestActivity.getInstance().clearApplicationData();
+        
+    	//keep        
+    	super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
     }
+	
+
+    
+//remove to end in production
+	public static FacebookTestActivity getInstance() {
+		return instance;
+	}
+
+	public void clearApplicationData() {
+		File cache = getCacheDir();
+		File appDir = new File(cache.getParent());
+		if (appDir.exists()) {
+			String[] children = appDir.list();
+			for (String s : children) {
+				if (!s.equals("lib")) {
+					deleteDir(new File(appDir, s));
+					Log.i("TAG", "**************** File /data/data/APP_PACKAGE/" + s + " DELETED *******************");
+				}
+			}
+		}
+	}
+
+	public static boolean deleteDir(File dir) {
+		if (dir != null && dir.isDirectory()) {
+			String[] children = dir.list();
+			for (int i = 0; i < children.length; i++) {
+				boolean success = deleteDir(new File(dir, children[i]));
+				if (!success) {
+					return false;
+				}
+			}
+		}
+
+		return dir.delete();
+	}
+	
 }
